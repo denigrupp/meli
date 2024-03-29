f93d8de0c68e1ba74e9ea72ec50060a8f811822b
denigrupp <158573509+denigrupp@users.noreply.github.com>
2024-03-29 04:42:19 -0300

    Update create_tables.sql

--- meli_1/scripts/create_tables.sql
+++ meli_1/scripts/create_tables.sql
@@ -1,73 +1,125 @@
 -- SELLER.CATEGORY definition
 
-CREATE TABLE `CATEGORY` (
-  `id_category` MEDIUMINT NOT NULL AUTO_INCREMENT,
-  `cod_category` bigint DEFAULT NULL,
-  `category` text,
-  `type` text
-) ENGINE=Inno
+CREATE TABLE CATEGORY (
+  id_category int NOT NULL AUTO_INCREMENT,
+  cod_category int DEFAULT NULL,
+  category int,
+  type int
+)  
 
 -- SELLER.CUSTOMER definition
 
-CREATE TABLE `CUSTOMER` (
-  `id_customer` MEDIUMINT NOT NULL AUTO_INCREMENT,
-  `email` text,
-  `name` text,
-  `last_name` text,
-  `address` text,
-  `birth_date` date,
-  `type` bigint DEFAULT NULL,
-  `seller_flg` bigint DEFAULT NULL,
-  `sexo` text,
+CREATE TABLE CUSTOMER (
+  id_customer int NOT NULL AUTO_INCREMENT,
+  email int,
+  name int,
+  last_name int,
+  address int,
+  birth_date date,
+  type int DEFAULT NULL,
+  seller_flg int DEFAULT NULL,
+  sexo int,
 	 PRIMARY KEY (id_customer)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+)  
 
 
 -- SELLER.ITEM definition
 
-CREATE TABLE `ITEM` (
-  `id_tem` MEDIUMINT NOT NULL AUTO_INCREMENT,
-  `sku` bigint DEFAULT NULL,
-  `product_name` text,
-  `start_date` date,
-  `close_date` date,
-  `active_flg` bigint DEFAULT NULL,
-  `id_category` bigint DEFAULT NULL,
+CREATE TABLE ITEM (
+  id_tem int NOT NULL AUTO_INCREMENT,
+  sku int DEFAULT NULL,
+  product_name int,
+  start_date date,
+  close_date date,
+  active_flg int DEFAULT NULL,
+  id_category int DEFAULT NULL,
 	 PRIMARY KEY (id_tem)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) 
 
--- SELLER.`ORDER` definition
+-- SELLER.ORDER definition
 
-CREATE TABLE `ORDER` (
-  `order_id` MEDIUMINT NOT NULL AUTO_INCREMENT
-  `order_code` bigint DEFAULT NULL,
-  `date` date,
-  `status` text,
-  `total_value` bigint DEFAULT NULL,
-  `taxes` bigint DEFAULT NULL,
-  `id_customer` bigint DEFAULT NULL,
-  `id_seller` bigint DEFAULT NULL,
+CREATE TABLE ORDER (
+  order_id int NOT NULL AUTO_INCREMENT
+  order_code int DEFAULT NULL,
+  date date,
+  status int,
+  total_value int DEFAULT NULL,
+  taxes int DEFAULT NULL,
+  id_customer int DEFAULT NULL,
+  id_seller int DEFAULT NULL,
 	PRIMARY KEY (order_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+   cart_id int
+)  
 
 
 -- SELLER.ORDER_ITEM definition
 
-CREATE TABLE `ORDER_ITEM` (
-  `order_id` INT,
-  `id_item` double DEFAULT NULL,
-  `quantity` double DEFAULT NULL,
-  `value` double DEFAULT NULL,
+CREATE TABLE ORDER_ITEM (
+ order_item_id int NOT NULL AUTO_INCREMENT,	
+  order_id INT,
+  id_item double DEFAULT NULL,
+  quantity double DEFAULT NULL,
+  value double DEFAULT NULL,
 	PRIMARY KEY (order_id,id_item)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+)  
 
 
 -- SELLER.PHONE definition
 
-CREATE TABLE `PHONE` (
-  `id_phone` MEDIUMINT NOT NULL AUTO_INCREMENT,
-  `number` bigint DEFAULT NULL,
-  `type` bigint DEFAULT NULL,
-  `customer_id` bigint DEFAULT NULL,
+CREATE TABLE PHONE (
+  id_phone int NOT NULL AUTO_INCREMENT,
+  number int DEFAULT NULL,
+  type int DEFAULT NULL,
+  customer_id int DEFAULT NULL,
 		PRIMARY KEY (id_phone)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+)  
+
+CREATE TABLE PAYMENT (
+	id_payment int  NOT NULL AUTO_INCREMENT,
+	payment varchar(299)  NOT NULL 
+	)
+CREATE TABLE ORDER_PAYMENT (
+	id_order_payment int  NOT NULL AUTO_INCREMENT,
+	id_order int,
+	id_payment int
+	)
+
+CREATE TABLE SELLER (
+  id_seller int NOT NULL AUTO_INCREMENT,
+  email int,
+  name int,
+  last_name int,
+  address int,
+  birth_date date,
+  type int DEFAULT NULL,
+  seller_flg int DEFAULT NULL,
+  sexo int,
+	 PRIMARY KEY (id_customer)
+) 
+
+CREATE TABLE ORDER_ITEM_SHIPPING (
+  order_shipping_id int NOT NULL AUTO_INCREMENT,
+  order_item_id int,
+  expected_date date,
+  delivery_date date
+)  
+
+CREATE TABLE CART
+(
+cart_id INT NOT NULL AUTO_INCREMENT,
+cart_value decimal(20,4),
+status varchar(100))	
+
+CREATE TABLE CART_ITEM
+(
+cart_id INT NOT NULL AUTO_INCREMENT,
+product_id int,
+quantity decimal(10,2))	
+
+CREATE TABLE PRODUCT_IMAGE
+(
+image_id INT NOT NULL AUTO_INCREMENT,
+product_id int,
+description text)
+
+	
